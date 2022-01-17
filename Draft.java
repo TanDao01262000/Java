15fbbba551697ebdf58b7d9252d9a1f81fbac28d
tankhanhdao <tankhanhdao@gmail.com>
2022-01-17 13:36:49 -0800

    add

---
+++ Draft.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Draft {
+    public static void main(String[] args ){
+        int i = 0;
+        int num = 0;
+
+        while (i<4){
+            num = (i++ *5);
+        }
+        System.out.println(num);
+    }
+}
